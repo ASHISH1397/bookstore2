6440df963f12ba1cfc5fcb628bfcad7ee1a9c904
qrquf <siddharthsrivastava117@gmail.com>
2024-04-01 09:43:17 -0700

    adding some more javascript features

--- style2.js
+++ style2.js
@@ -1,3 +1,67 @@
+function addfunction()
+{
+    var r=new Map();
+    r.set('I',["Operating Systems","C","D.C.O","Computer Networks"]);
+    r.set('II',["Artificial Intelligence","Artificial Intelligence","Artificial Intelligence","Data Structure and Algorithms"]);
+    r.set('III',["Machine Learning","Computer Forensics","Computer Forensics","Programming in C++"]);
+    r.set('IV',["Programming in Java","Programming in Java","Programming in Python","Hydraulic Structures"]);
+    r.set('V',["Soil Mechanics","Transportation Engineering","Transportation Engineering","Structural Mechanics"]);
+    r.set('VI',["Water Resources Engineering","Hydropower Engineering","Forensic Civil Engineering","Bridge Engineering"]);
+    r.set('VII',["Groundwater Engineering","Pavement Engineering","Traffic Engineering","Data Analysis and Interpretation"]);
+    r.set('VIII',["Engineering Graphics and Drawing","Applied Thermodynamics","Electric Circuits","Fluid Mechanics"]);
+   var c=document.getElementById("semesterList");
+   var z=document.getElementById("subjectsList");
+   var a=c.childNodes;
+   console.log(a.length);
+   for(i=0;i<a.length;i++)
+   {
+    var ch=a[i].childNodes
+    console.log(ch[1].nodeName);
+    if(ch[1].checked)
+    {
+        console.log(ch[1].value);
+        var q=r.get(ch[1].value);
+      
+        for(j=0;j<q.length;j++)
+        {
+            console.log(q[j]);
+            var x=document.createElement('LI');
+            var y=document.createElement('input');
+            x.innerHTML=q[j];
+            y.setAttribute('type','radio');
+            y.setAttribute('name','semester');
+            y.setAttribute('id',q[j]);
+            x.appendChild(y);
+            z.appendChild(x);
+        }
+        
+    }
+   }
+}
+function removedata2()
+{    
+    var e=document.getElementById("subjectsList");
+    var child=e.childNodes;
+    var s=child.length;
+    console.log(s);
+    for(i=0;i<s;i++)
+    {
+        var d=child[0];
+        console.log(d.nodeName);
+       console.log(i);
+       if(d.nodeName==="#text")
+       {
+
+       e.removeChild(d);
+       }
+        if(d.nodeName==="LI")
+        {
+        var q=d.childNodes;
+        d.removeChild(q[0]);
+        e.removeChild(d);
+    }
+}
+}
 function removedata()
 {    
     var e=document.getElementById("semesterList");
@@ -11,6 +75,7 @@ function removedata()
        console.log(i);
        if(d.nodeName==="#text")
        {
+
        e.removeChild(d);
        }
         if(d.nodeName==="LI")
@@ -58,6 +123,7 @@ x.set('mtech_chemical',["I","II","III","IV"]);
                 x.innerHTML=z[j];
                 y.setAttribute('type','radio');
                 y.setAttribute('name','semester');
+                y.setAttribute('onclick','removedata2();addfunction();');
                 y.setAttribute('value',z[j]);
                 x.appendChild(y);
                 sid.appendChild(x);
